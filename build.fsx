54c8659a838c36a2b14657ef3c5eafe4e6664cde
Matthias Dittrich <matthi.d@gmail.com>
2016-05-25 00:49:18 +0200

    almost there

--- build.fsx
+++ build.fsx
@@ -6,6 +6,7 @@
 #r @"packages/build/FAKE/tools/FakeLib.dll"
 #r @"packages/build/sharpcompress/lib/net40/SharpCompress.dll"
 
+
 open System
 open System.IO
 open System.Net
@@ -14,6 +15,8 @@ open SharpCompress
 open SharpCompress.Reader
 open SharpCompress.Archive
 open SharpCompress.Common
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__ 
 
 /// Run the given buildscript with FAKE.exe
 let executeWithOutput configStartInfo =
@@ -56,6 +59,33 @@ let ipyW workingDir args =
       setVar "PYTHONPATH" (Path.GetFullPath "temp/IronPython"))
 let ipy args = ipyW "temp/IronPython" args
 
+let paketPath = ".paket" @@ "paket.exe"
+let paketStartInfo workingDirectory args =
+    (fun (info: System.Diagnostics.ProcessStartInfo) ->
+        info.FileName <- System.IO.Path.GetFullPath paketPath
+        info.Arguments <- sprintf "%s" args
+        info.WorkingDirectory <- workingDirectory
+        let setVar k v =
+            info.EnvironmentVariables.[k] <- v
+        setVar "MSBuild" msBuildExe
+        setVar "GIT" Git.CommandHelper.gitPath
+        setVar "FSI" fsiPath)
+
+let fakePath = "packages" @@ "build" @@ "FAKE" @@ "tools" @@ "FAKE.exe"
+let fakeStartInfo script workingDirectory args fsiargs environmentVars =
+    (fun (info: System.Diagnostics.ProcessStartInfo) ->
+        info.FileName <- System.IO.Path.GetFullPath fakePath
+        info.Arguments <- sprintf "%s --fsiargs -d:FAKE %s \"%s\"" args fsiargs script
+        info.WorkingDirectory <- workingDirectory
+        let setVar k v =
+            info.EnvironmentVariables.[k] <- v
+        for (k, v) in environmentVars do
+            setVar k v
+        setVar "MSBuild" msBuildExe
+        setVar "GIT" Git.CommandHelper.gitPath
+        setVar "FSI" fsiPath)
+
+
 Target "SetupIronPython" (fun _ ->
     CleanDir "temp/IronPython"
     CopyDir ("temp"@@"IronPython") ("packages"@@"IronPython"@@"lib"@@"Net45") (fun _ -> true)
@@ -64,6 +94,35 @@ Target "SetupIronPython" (fun _ ->
     CopyDir ("temp"@@"IronPython") ("packages"@@"IronPython.StdLib"@@"content") (fun _ -> true)
     ipy "-X:Frames -m ensurepip"
 
+    let installPackageE ext name version md5 =
+      let targetFile = sprintf "temp/IronPython/%s-%s%s" name version ext
+      downloadFile
+        targetFile
+        (sprintf "https://pypi.python.org/packages/source/p/%s/%s-%s%s#md5=%s" name name version ext md5)
+        |> Async.RunSynchronously
+      let distDir = "temp/IronPython/dist" // sprintf "temp/IronPython/%s-%s" name version
+      let targetDir = sprintf "%s/%s-%s" distDir name version
+      CleanDir targetDir
+      extract targetDir targetFile
+      let containsSetup dir = File.Exists (sprintf "%s/setup.py" dir)
+      if containsSetup targetDir then
+        ipyW targetDir "-X:Frames setup.py install"
+      else
+        let subDir = sprintf "%s/%s-%s" targetDir name version
+        if containsSetup subDir then
+          ipyW subDir "-X:Frames setup.py install"
+        else
+          failwith "Could not find setup.py in package!"
+    let installPackage = installPackageE ".tar.gz"
+
+    // Install patch, such that we can apply our patches :)
+    installPackageE ".zip" "patch" "1.16" "dbcbbd4e45ddd8baeb02bddf663a3176"
+    
+    let patch patchFile =
+      ipy (sprintf "-X:Frames -m patch -v ../../patches/%s" patchFile)
+
+    patch "patch_pip.patch"
+    
     // install protobuf manually
     downloadFile
       "temp/IronPython/protobuf-3.0.0a3-py2-none-any.whl"
@@ -71,39 +130,41 @@ Target "SetupIronPython" (fun _ ->
       |> Async.RunSynchronously
     ipy "-X:Frames -m pip install protobuf-3.0.0a3-py2-none-any.whl"
 
-    let installPackage name version md5 =
-      let targetFile = sprintf "temp/IronPython/%s-%s.tar.gz" name version
-      downloadFile
-        targetFile
-        (sprintf "https://pypi.python.org/packages/source/p/%s/%s-%s.tar.gz#md5=%s" name name version md5)
-        |> Async.RunSynchronously
-      let targetDir = "temp/IronPython/dist" // sprintf "temp/IronPython/%s-%s" name version
-      CleanDir (sprintf "%s/%s-%s" targetDir name version)
-      extract targetDir targetFile
-      ipyW (sprintf "%s/%s-%s" targetDir name version) "-X:Frames setup.py install"
-      
     // install pyasn1 manually
     installPackage "pyasn1" "0.1.8" "7f6526f968986a789b1e5e372f0b7065"
-    //downloadFile
-    //  "temp/IronPython/pyasn1-0.1.8.tar.gz"
-    //  "https://pypi.python.org/packages/source/p/pyasn1/pyasn1-0.1.8.tar.gz#md5=7f6526f968986a789b1e5e372f0b7065"
-    //  |> Async.RunSynchronously
-    //CleanDir "temp/IronPython/pyasn1-0.1.8"
-    //extract "temp/IronPython/pyasn1-0.1.8" "temp/IronPython/pyasn1-0.1.8.tar.gz"
-    //ipy "-X:Frames pyasn1-0.1.8/setup.py install"
 
     // install pyasn1-modules manually
     installPackage "pyasn1-modules" "0.0.6" "3b94e7a4999bc7477b76c46c30a56727"
-    //downloadFile
-    //  "temp/IronPython/pyasn1-modules-0.0.6.tar.gz"
-    //  "https://pypi.python.org/packages/source/p/pyasn1-modules/pyasn1-modules-0.0.6.tar.gz#md5=3b94e7a4999bc7477b76c46c30a56727"
-    //  |> Async.RunSynchronously
-    //CleanDir "temp/IronPython/pyasn1-modules-0.0.6"
-    //extract "temp/IronPython/pyasn1-modules-0.0.6" "temp/IronPython/pyasn1-modules-0.0.6.tar.gz"
-    //ipy "-X:Frames pyasn1-modules-0.0.6/setup.py install"
 
     // Install pycrypto
     ipy "-X:Frames -m easy_install http://www.voidspace.org.uk/downloads/pycrypto26/pycrypto-2.6.win32-py2.7.exe"
+    CopyDir 
+      ("temp"@@"IronPython"@@"Lib"@@"site-packages"@@"Crypto")
+      ("temp"@@"IronPython"@@"Lib"@@"site-packages"@@"pycrypto-2.6-py2.7-cli.egg"@@"Crypto")
+      (fun _ -> true)
+    
+    Git.Repository.cloneSingleBranch
+      ("temp")
+      ("https://github.com/matthid/ironpycrypto.git")
+      "master"
+      "ironpycrypto"
+    execute
+      "Restoring packages for ironpycrypto"
+      "Restoring packages for ironpycrypto failed"
+      (paketStartInfo "temp/ironpycrypto" "restore")
+    execute
+      "Building ironpycrypto, this could take some time, please wait..."
+      "building ironpycrypto failed"
+      (fakeStartInfo "build.fsx" "temp/ironpycrypto" "" "" [])
+    CopyDir
+      ("temp"@@"IronPython"@@"Lib"@@"site-packages"@@"Crypto")
+      ("temp"@@"ironpycrypto"@@"Crypto")
+      (fun _ -> true)
+    CopyFile 
+      ("temp"@@"IronPython") 
+      ("temp"@@"ironpycrypto"@@"IronPyCrypto"@@"bin"@@"Release"@@"IronPyCrypto.dll")
+
+    DeleteDir ("temp"@@"IronPython"@@"Lib"@@"site-packages"@@"pycrypto-2.6-py2.7-cli.egg")
 
     ipy "-X:Frames -m pip install http"
     ipy "-X:Frames -m pip install gmusicapi"
